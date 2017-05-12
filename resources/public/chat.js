41f2040882a6e2e4075f45e9ac16a409cb03c160
Ben Sterrett <raynjamin@gmail.com>
2017-05-12 16:35:21 -0400

    Adding some comments

--- resources/public/chat.js
+++ resources/public/chat.js
@@ -1,9 +1,14 @@
 function addMessage() {
+    // builds a message JS object
     var message = {
         author: $("#author").val(),
         text: $("#text").val()
     };
 
+    // POSTS this object to /add-message
+    // the object needs to be stringified first.
+    // the anonymous function will run when the POST
+    // request completes.
     $.post(
         "/add-message",
         JSON.stringify(message),
@@ -14,13 +19,26 @@ function addMessage() {
 }
 
 function getMessages() {
+    // using jquery to issue a GET request to /get-messages
     $.get(
         "/get-messages",
+
+        // this anonymous function will run with the request completes
+        // "data" will be the returned data
         function(data) {
+
+            // clearing out the area of the
+            // page that is the messagaes
             $("#messages").empty();
 
+            // parsing the resultant data into json
             var messages = JSON.parse(data);
 
+
+            // looping through the returned messages
+            // creating <div> elements
+            // appending them to the messages area of the
+            // page
             for (var i in messages) {
                 var author = messages[i].author;
                 var text = messages[i].text;
@@ -32,4 +50,6 @@ function getMessages() {
     );
 }
 
+// setInterval runs "getMessages" every "1000" ms
+// or, ever 1 s.
 setInterval(getMessages, 1000);
